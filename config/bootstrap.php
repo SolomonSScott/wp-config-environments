1ffed485cc8335be43a27782e4a4c7feb20a0df7
Solomon Scott <solomon.s.scott@gmail.com>
2017-02-02 21:02:35 -0500

    Initial Commit

---
+++ config/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+  if (preg_match("/^((192|10|127)\.)|::1/", $_SERVER['REMOTE_ADDR']) ) {
+    require_once 'environments/development.php';
+  } else {
+    require_once 'environments/production.php';
+  }
+  define('DB_NAME',     $WP_ENVIRONMENT['db_name']);
+  define('DB_USER',     $WP_ENVIRONMENT['db_user']);
+  define('DB_PASSWORD', $WP_ENVIRONMENT['db_password']);
+  define('DB_HOST',     $WP_ENVIRONMENT['db_host']);
+  define('WP_DEBUG',    $WP_ENVIRONMENT['wp_debug']);
+  define('WPBP_ENV',    $WP_ENVIRONMENT['name']);
+?>
---
+++ config/environments/development.php
@@ -0,0 +1,11 @@
+<?php
+  $WP_ENVIRONMENT = array(
+    'db_name' => '',
+    'db_user' => 'root',
+    'db_password' => '',
+    'db_host' => 'localhost',
+    'wp_lang' => '',
+    'wp_debug' => true,
+    'name' => 'development'
+  );
+?>
